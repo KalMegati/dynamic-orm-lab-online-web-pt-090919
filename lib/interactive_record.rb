a942f0c0f6fccc8e48f648fa4d751f592b3d8ed2
Srikalyan Megati <kalmegati@gmail.com>
2019-11-16 21:39:21 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -8,20 +8,23 @@ class InteractiveRecord
     self.to_s.downcase.pluralize
   end
   
-def self.column_names
-  #DB[:conn].results_as_hash = true
- 
-  sql = "PRAGMA table_info('#{table_name}')"
- 
-  table_info = DB[:conn].execute(sql)
-  binding.pry
-  column_names = []
- 
-  table_info.each do |column, value|
-    column_names << column["name"]
+  def self.column_names
+    #DB[:conn].results_as_hash = true
+   
+    sql = "PRAGMA table_info('#{table_name}')"
+   
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+   
+    table_info.each do |column, value|
+      column_names << column["name"]
+    end
+   
+    column_names.compact
   end
- 
-  column_names.compact
-end
+  
+  def initialize
+    
+  end
   
 end
