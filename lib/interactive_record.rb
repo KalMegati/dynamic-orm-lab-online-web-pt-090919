3c9fe5aced6b09e08121ad36060397131a4a7818
Srikalyan Megati <kalmegati@gmail.com>
2019-11-16 22:41:02 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -59,7 +59,7 @@ class InteractiveRecord
     DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = '#{name}'")
   end
   
-  def self.find_by
+  def self.find_by(kv_pair)
     
   end
   
