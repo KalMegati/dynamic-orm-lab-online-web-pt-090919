d11e04822673e7d1fbd8fa60ba2c77c6348f7a87
Srikalyan Megati <kalmegati@gmail.com>
2019-11-16 21:36:04 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -9,7 +9,7 @@ class InteractiveRecord
   end
   
 def self.column_names
-  DB[:conn].results_as_array = true
+  DB[:conn].results_as_array = false
  
   sql = "PRAGMA table_info('#{table_name}')"
  
