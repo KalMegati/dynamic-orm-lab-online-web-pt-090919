b39436845510d2e8c5a819f84c47dd89d9a81e0f
Srikalyan Megati <kalmegati@gmail.com>
2019-11-16 21:32:05 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -15,7 +15,7 @@ def self.column_names
   table_info = DB[:conn].execute(sql)
   column_names = []
  
-  table_info.each do |column, value|
+  table_info.each do |column, value, value|
     column_names << column["name"]
   end
  
